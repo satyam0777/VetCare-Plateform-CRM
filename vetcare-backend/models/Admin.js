7e8627392db15f8452d55e3e111b2e0203ce53b1
satyam0777 <officialsatyam0777@gmail.com>
2025-10-04 21:17:23 +0530

    admin now see the user info

---
+++ vetcare-backend/models/Admin.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const adminSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  userIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // Array of user IDs
+  // ...other admin fields
+});
+
+module.exports = mongoose.model('Admin', adminSchema);
